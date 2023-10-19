9a40be4f34e273428dc988423d084abb16621435
Tim Slater <tim.slater@intrasee.com>
2023-10-19 18:52:09 -0500

    Display genres

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,16 +1,26 @@
-import React from "react";
-import useData from "../hooks/useData";
 import useGenres, { Genre } from "../hooks/useGenres";
+import { HStack, Image, List, ListItem, Text } from "@chakra-ui/react";
+import getCroppedImageUrl from "../services/image-url";
 
 const GenreList = () => {
   const { data } = useGenres();
 
   return (
-    <ul>
+    <List>
       {data.map((genre) => (
-        <li key={genre.id}>{genre.name}</li>
+        <ListItem key={genre.id} paddingY="5px">
+          <HStack>
+            <Image
+              boxSize="32px"
+              borderRadius={8}
+              src={getCroppedImageUrl(genre.image_background)}
+              alt={genre.name}
+            />
+            <Text fontSize="lg">{genre.name}</Text>
+          </HStack>
+        </ListItem>
       ))}
-    </ul>
+    </List>
   );
 };
 

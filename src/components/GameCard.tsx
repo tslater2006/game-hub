536646356a05e585fd42035ae91667b5cd09c7e1
Tim Slater <tim.slater@intrasee.com>
2023-10-21 15:27:39 -0500

    Add Emojis

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -12,6 +12,7 @@ import {
 import PlatformIconList from "./PlatformIconList";
 import CriticScore from "./CriticScore";
 import getCroppedImageUrl from "../services/image-url";
+import Emoji from "./Emoji";
 
 interface GameCardProps {
   game: Game;
@@ -28,7 +29,10 @@ const GameCard = ({ game }: GameCardProps) => {
           />
           <CriticScore score={game.metacritic} />
         </HStack>
-        <Heading fontSize="2xl">{game.name}</Heading>
+        <Heading fontSize="2xl">
+          {game.name}
+          <Emoji rating={game.rating_top} />{" "}
+        </Heading>
       </CardBody>
     </Card>
   );
